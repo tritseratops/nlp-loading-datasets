0c2e5fb40014beec6bed7f3807d8be8b4e99feda
Oleksandr Yanov <oleksandr.yanov@gmail.com>
2022-11-05 07:17:39 +0200

    finished loading data, got 13k question in dev version and 130k in train

--- squad20.py
+++ squad20.py
@@ -2,11 +2,13 @@ import json
 
 
 # read json and count stories and avg questions
-filepath = "../../NLP/squad20/dev-v2.0.json"
+filepath_dev = "../../NLP/squad20/dev-v2.0.json"
+filepath_train = "../../NLP/squad20/train-v2.0.json"
 # count yes/no questions based uif there is yes or no answer
 
 # Opening JSON file
-f = open(filepath)
+# f = open(filepath_dev)
+f = open(filepath_train)
 
 # structure data/paragraphs/qas/  - whole doc/ story/ question and answer
 
@@ -14,14 +16,30 @@ f = open(filepath)
 # a dictionary
 json_data = json.load(f)
 # item_dict `= json.loads(json_data)
-print(len(json_data['data']))
 #
 
 
 # Iterating through the json
 # list
-# for i in data['emp_details']:
-#     print(i)
+
+total_fields = json_data['data']
+total_questions = 0
+total_paragraphs = 0
+for data_fields in total_fields:
+    print(data_fields["title"])
+    paragraphs = len(data_fields)
+    print("Paragraphs:", paragraphs)
+    total_paragraphs += paragraphs
+    data_field_questions = 0
+    for paragraph in data_fields["paragraphs"]:
+        p_questions = len(paragraph["qas"])
+        data_field_questions += p_questions
+    print("Data Field questions: ", data_field_questions)
+    total_questions += data_field_questions
+
+print("Fields:", len(total_fields))
+print("Total Paragraphs: ", total_paragraphs)
+print("Total questions: ", total_questions)
 
 # Closing file
 f.close()
